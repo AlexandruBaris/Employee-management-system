233d97e788c76f42b8be32249d0816e17ce113d9
AlexandruBaris <a.barisenschi@gmail.com>
2021-06-11 15:24:22 +0300

    first commit

---
+++ emp-server/src/main/java/com/example/empserver/EmpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.empserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpServerApplication.class, args);
+	}
+
+}
---
+++ emp-server/src/test/java/com/example/empserver/EmpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
